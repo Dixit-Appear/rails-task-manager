a9980cdcb329159850d5a664e5a3b68540aea489
Edoardo Olivieri <diddo.42@hotmail.it>
2019-02-19 16:09:43 +0000

    dry code

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2019_02_19_120144) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "compleated", default: false
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
